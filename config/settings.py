43a9e3c8a6a135c81765bc64eca7d3971f050606
Tiago Fernandes <tiagoferprojects@gmail.com>
2025-02-02 12:53:27 -0300

    b

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3vh-i*nj_byrbhqyud4fd)*utt88-&!9caizsx9s8s3z6ty2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
