f26ca200643eef977652fa05029647d7a721f85e
Tiago Fernandes <tiagoferprojects@gmail.com>
2025-02-03 21:18:13 -0300

    b

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import PublicUserDetailView
+from .views import PublicUserDetailView, update_want_collect
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/<int:user_id>/', PublicUserDetailView.as_view(), name='public_user_detail'),
+    path('api/update_want_collect/<int:user_id>/', update_want_collect, name='update_want_collect'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.http import JsonResponse
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
 
 class PublicUserDetailView(APIView):
     def get(self, request, user_id):
@@ -11,3 +16,24 @@ class PublicUserDetailView(APIView):
             return Response(serializer.data)
         except CustomUser.DoesNotExist:
             return Response({"error": "Usuário não encontrado"}, status=404)
+
+@csrf_exempt
+@require_POST
+def update_want_collect(request, user_id):
+    try:
+        user = CustomUser.objects.get(id=user_id)  # Encontre o usuário pelo ID
+        data = json.loads(request.body)
+        
+        # Verifique se 'want_collect' foi enviado na requisição
+        if 'want_collect' in data:
+            user.want_collect = data['want_collect']
+            user.save()
+            return JsonResponse({'success': True, 'want_collect': user.want_collect}, status=200)
+        else:
+            return JsonResponse({'error': 'Campo "want_collect" não fornecido'}, status=400)
+
+    except CustomUser.DoesNotExist:
+        return JsonResponse({'error': 'Usuário não encontrado'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Erro ao processar JSON'}, status=400)
+
